d74f9676ee7e3fa2d25c1ed2b8478b820b701a09
David Kundih <kundihdavid@gmail.com>
2021-11-03 23:25:04 +0100

    metadata

--- duality/__init__.py
+++ duality/__init__.py
@@ -3,6 +3,14 @@ from duality.eoq import eoq
 from duality.hub import hub
 from duality.misc.global_functions import *
 from duality.montecarlo import montecarlo
+from duality.misc._meta import (
+	__author__,
+	__copyright__,
+	__credits__,
+	__license__,
+	__version__,
+)
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
