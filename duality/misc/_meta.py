006bca9c7c145d7f05d8e55c8a926e7d2caf451b
David Kundih <kundihdavid@gmail.com>
2021-11-08 12:17:37 +0100

    _meta change

--- duality/misc/_meta.py
+++ duality/misc/_meta.py
@@ -9,11 +9,11 @@ __credits__ = [
 	'dkundih <https://github.com/dkundih>',
 
 ]
-__license__ = 'Copyright (c) 2021- David Kundih. All rights reserved.\nLicensed under the Apache License, Version 2.0.\n\nFor more details about the license and terms of use visit the official duality documentation linked at https://github.com/dkundih/duality and https://pypi.org/project/duality'
+__license__ = 'Copyright (c) 2021- David Kundih. All rights reserved.\nLicensed under the Apache License, Version 2.0. | For more details about the license and terms of use visit the official duality documentation linked at https://github.com/dkundih/duality and https://pypi.org/project/duality'
 __version__ = '3.1.4'
-__documentation__ = 'https://github.com/dkundih/duality\nhttps://pypi.org/project/duality'
-__contact__ = 'dakundih@unin.hr\nkundihdavid@gmail.com'
-__donate__ = 'https://patreon.com/dkundih\nhttps://buymeacoffee.com/dkundih'
+__documentation__ = 'https://github.com/dkundih/duality | https://pypi.org/project/duality'
+__contact__ = 'dakundih@unin.hr | kundihdavid@gmail.com'
+__donate__ = 'https://patreon.com/dkundih | https://buymeacoffee.com/dkundih'
 
 #general setup confirmation.
 def main():
