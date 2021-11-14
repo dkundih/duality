d024a5e59f4581c304d977bd529dcb8d5185b309
David Kundih <kundihdavid@gmail.com>
2021-11-14 13:31:29 +0100

    3.2.1 push

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
+  'Intended Audience :: Science/Research',
+  'Intended Audience :: Customer Service',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Operating System :: Microsoft :: Windows :: Windows 7',
+  'Operating System :: Microsoft :: Windows :: Windows 8',
+  'Operating System :: Microsoft :: Windows :: Windows 8.1',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: Apache Software License',
   'Programming Language :: Python',
@@ -15,7 +21,13 @@ classifiers = [
    'Programming Language :: Python :: 3.10',
     'Topic :: Database',
     'Topic :: Education',
-    'Topic :: Office/Business'
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Office/Business :: Financial :: Investment',
+    'Topic :: Office/Business :: Financial :: Accounting',
+    'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+    'Topic :: Office/Business :: Financial :: Spreadsheet'
+
 ]
  
 #import current duality version.
@@ -29,8 +41,15 @@ setup(
   long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(), 
   author = 'David Kundih',
   author_email = 'kundihdavid@gmail.com',
+  maintainer = 'David Kundih',
+  maintainer_email = 'kundihdavid@gmail.com',
   url = 'http://github.com/dkundih/duality',
-  license = 'Apache Software License', 
+  download_url = f'https://github.com/dkundih/duality/archive/refs/tags/v{__version__}.tar.gz',
+  license = 'Apache Software License',
+  project_urls = {
+    'Documentation': 'https://github.com/dkundih/duality/blob/main/README.md',
+    'Source Code': 'https://github.com/dkundih/duality/tree/main/duality'
+  }, 
   classifiers = classifiers,
   keywords = 'data science, machine learning, data manipulation, artificial intelligence, AI, duality, duality-py, duality.py',
   packages = find_packages(),
