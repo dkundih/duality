e65ab4105b373e4907a2fd1827bc893de24738c9
David Kundih <kundihdavid@gmail.com>
2021-11-20 12:55:53 +0100

    Update __init__.py

--- duality/__init__.py
+++ duality/__init__.py
@@ -14,23 +14,23 @@ AVAILABLE FEATURES IN THE LIBRARY:
 		print(help(any_function_listed_below)) in order to see the function details or print(help(duality.toolkit)) for all functions at once.
 
 		FUNCTIONS (ACCESSIBLE DIRECTLY FROM THE LIBRARY)
-		---------
+		------------------------------------------------
 
 		random_value(mean, st_dev, **rounded) - gives a random value of mean and standard deviation inputed, if rounded = 'y', value will be rounded.
 
-        random_pool(mean, st_dev, pool_size, **rounded) - gives random values of mean and standard deviation inputed for the amount of values defined in the pool size, if rounded = 'y', values will be rounded.
+		random_pool(mean, st_dev, pool_size, **rounded) - gives random values of mean and standard deviation inputed for the amount of values defined in the pool size, if rounded = 'y', values will be rounded.
 
-        split_values(data, split_method) - splits the data using a split method character.
+		split_values(data, split_method) - splits the data using a split method character.
 
-        join_values(data, join_method) - joins the data using a join metod character.
+		join_values(data, join_method) - joins the data using a join metod character.
 
-        replace_values(data, replaced_value, replacing_value) - replaces a defined value with a desired value.
+		replace_values(data, replaced_value, replacing_value) - replaces a defined value with a desired value.
 
-        list_sort(data, array) - manually sorts data depending on defined array of indexes.
+		list_sort(data, array) - manually sorts data depending on defined array of indexes.
 
-        index_sort(data, split_method, index_array) - sorts the indicies in a list of values based on the index array defined as [x,x,x].
+		index_sort(data, split_method, index_array) - sorts the indicies in a list of values based on the index array defined as [x,x,x].
 
-        auto_sort(data, split_method, trigger = lambda x: x[0]) - automatically splits all values in a list and sorts them based on the added trigger as lambda x: [x[i], x[i]] and joins them back together.
+		auto_sort(data, split_method, trigger = lambda x: x[0]) - automatically splits all values in a list and sorts them based on the added trigger as lambda x: [x[i], x[i]] and joins them back together.
 
 	MONTECARLO (OBJECT)
 	-------------------
