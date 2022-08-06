0b43cbbabc3c8256e2af86bea88a714d17c49706
David Kundih <kundihdavid@gmail.com>
2022-08-07 01:36:09 +0200

    paint_text added

---
+++ _testassets/D_colordict.py
@@ -0,0 +1,22 @@
+from colorama import (Fore,
+                      init,
+                      )
+
+init()
+
+def paint_text(text, color, print_trigger = True):
+    colors = {'r': Fore.RED,
+              'g' : Fore.GREEN,
+              'b' : Fore.BLUE,
+              'k' : Fore.BLACK,
+              'm' : Fore.MAGENTA,
+              'y' : Fore.YELLOW,
+              'c' : Fore.CYAN,
+              }
+    if print_trigger == True:
+        return print(colors[color] + text + Fore.RESET)
+    
+    elif print_trigger == False:
+        return colors[color] + text + Fore.RESET
+        
+paint_text('tekst', 'g')
