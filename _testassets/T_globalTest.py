016ceb417b4365151e0b780d7c545c4a15c599d0
David Kundih <kundihdavid@gmail.com>
2022-08-07 16:07:12 +0200

    test confirmation

--- _testassets/T_globalTest.py
+++ _testassets/T_globalTest.py
@@ -10,7 +10,7 @@ class Car:
         self.speed = speed
         self.country = country
 
-    @app.entry(option_name = 'change speed', option_description = 'this changes the speed of the car.', print_val = True)
+    @app.entry(option_name = 'change speed', option_description = 'this changes the speed of the car.')
     def set_name(self, amount = app.store('amount', 'int')):
         self.speed += amount
         return self.speed
@@ -19,7 +19,6 @@ class Car:
     def show_info(self):
         return f'Car of the brand {self.brand}, speed {self.speed} from the country of {self.country}.'
 
-
 if __name__ == '__main__':
     app.config(type = 'dynamic', queue = True)
-
+    print('Executed on:', duality.__version__)
